b7ad70160c597b507d6fc36200832d8af23cc6f1
Sharan Yagneswar <sharan.yagneswar@mindmusiclabs.com>
2020-07-24 18:01:05 +0200

    Cosmetic changes and addition of licence info.

--- include/audio_control_protocol/device_control_protocol.h
+++ include/audio_control_protocol/device_control_protocol.h
@@ -1,13 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * @copyright 2017-2019 Modern Ancient Instruments Networked AB, dba Elk,
+ * Stockholm
+ */
+
 /**
- * Copyright (C) 2018, Mind Music Labs AB, Stockholm
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * @brief Contains the protocol that can be used by a driver in a host machine
+ *        to comunicate and control secondary microcontrollers.
  */
 #ifndef DEVICE_PROTOCOL_H_
 #define DEVICE_PROTOCOL_H_
 
+// Denotes the payload size in bytes that this protocol can carry
 #define DEVICE_PACKET_PAYLOAD_SIZE 8
 
 #ifndef __KERNEL__
@@ -28,16 +32,19 @@ enum device_commands {
  * @brief device control packet structure
  */
 struct device_control_packet {
-	//@ magic start chars 'x', 'i'
+	// magic start chars 'x', 'i'
 	uint8_t magic_start[2];
-	//@ command msb & lsb
+
+	// command msb & lsb
 	uint8_t device_cmd;
-	//@ command payload
+
+	// command payload
 	uint8_t payload[DEVICE_PACKET_PAYLOAD_SIZE];
-	//@ magic stop char 'd'
+
+	// magic stop char 'd'
 	uint8_t magic_stop;
 };
 
 #define DEVICE_CONTROL_PACKET_SIZE sizeof(struct device_control_packet)
 
-#endif
+#endif // DEVICE_PROTOCOL_H_
